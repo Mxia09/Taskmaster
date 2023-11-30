92324bd3fdeebb296e79164ca830b95119293cd4
Marvin Xia <marvinxia09@gmail.com>
2023-11-29 16:55:06 -0800

    Angular U

---
+++ frontend/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { TaskviewComponent } from './pages/taskview/taskview.component';
+
+export const routes: Routes = [
+    {path:'', component: TaskviewComponent}
+];
